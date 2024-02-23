223a4a261794b7f9982c9ebf09a03577e90890e4
jovantay521 <jovantay521@gmail.com>
2024-02-23 11:40:16 +0800

    moved client to src

--- src/RFC865UDPClient.java
+++ src/RFC865UDPClient.java
@@ -2,7 +2,7 @@ import java.net.*;
 
 // Name: Tay Jovan
 // Group: SCSC
-// IP Address:  
+// IP Address: 10.96.184.167
 
 public class RFC865UDPClient {
     public static void main(String[] args) {
@@ -12,15 +12,16 @@ public class RFC865UDPClient {
             // Create a datagram socket
             socket = new DatagramSocket();
 
-            // Specify the server address and port (localhost and port 13 in this case)
+            // Specify the server address and port (lab address and port 17)
             // InetAddress serverAddress = InetAddress.getByName("localhost");  //alternative        
-            // InetAddress localhost = InetAddress.getLocalHost();
-            InetAddress LAB_IP = InetAddress.getByName("swlab2-c.scse.ntu.edu.sg");
-            InetAddress serverAddress = LAB_IP;
-            int serverPort = 13;
+            InetAddress localhost = InetAddress.getLocalHost();
+            InetAddress lab_ip_address = InetAddress.getByName("swlab2-c.scse.ntu.edu.sg");
+            InetAddress serverAddress = lab_ip_address;
+            int serverPort = 17;
 
             // Create a request message (empty in this case)
-            String message = "TayJovan, SCSC, " + serverAddress.getHostAddress();
+            String message = "TAY JOVAN, SCSC, " + localhost.getHostAddress();
+            System.out.println("Message: " + message);
             byte[] requestData = message.getBytes();
 
             // Create a request packet
@@ -28,11 +29,13 @@ public class RFC865UDPClient {
 
             // Send the request to the server
             socket.send(requestPacket);
+            System.out.println("Packet sent\nWaiting response...");
 
             // Receive the response from the server
             byte[] responseData = new byte[256];
             DatagramPacket responsePacket = new DatagramPacket(responseData, responseData.length);
             socket.receive(responsePacket);
+            System.out.println("Received response");
 
             // Convert the response data to a string and display it
             String response = new String(responsePacket.getData(), 0, responsePacket.getLength());
